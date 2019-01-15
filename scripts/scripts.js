01208e2d6e5e5584aa99a328143d3180f4416fee
Kristen Kulha <kristen.m.kulha@gmail.com>
2019-01-15 11:14:29 -0800

    Add JS for smooth scroll to nav elements.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -17,3 +17,23 @@ function navStick() {
 }
 
 document.addEventListener('scroll', navStick);
+
+function scrollIt(element) {
+  window.scrollTo({
+    'behavior': 'smooth',
+    'left': 0,
+    'top': element.offsetTop - 55
+  });
+  console.log(element);
+}
+
+const about = document.querySelector('.about');
+const aboutDiv  =document.querySelector('#about');
+const matches = document.querySelector('.matches');
+const matchesDiv = document.querySelector('#matches');
+const gallery = document.querySelector('.gallery');
+const galleryDiv = document.querySelector('#gallery');
+
+about.addEventListener('click', () => scrollIt(aboutDiv));
+matches.addEventListener('click', () => scrollIt(matchesDiv));
+gallery.addEventListener('click', () => scrollIt(galleryDiv));
